b65f13ad857c7975d6c8a872904c5d2c53d09b9d
Uživatel Gates <misa.hladky@gmail.com>
2025-09-08 14:23:00 +0200

    lepsi jeden vpis u tz kamerz

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
         time.sleep(0.1)
 
     if frame_reader.frame is None:
-        print("Stream se nepodařilo spustit.")
+        print("[Stream] Stream se nepodařilo spustit.")
     else:
-        print("Stream běží.")
+        print("[Stream] Stream běží.")
     
     #-------------------nastaveni------------------------------
     height = 150 # nastaveni jak ma dron být vysko
@@ -34,7 +34,7 @@ if __name__ == "__main__":
         tello.move_up(height - take_off_height) # vzlet na pozadovanou vysku
 
         # hlavni beh dronu
-        drone_controller.interval_scan_loop(tello, frame_reader, step_cm=25.0, min_cov_pct=5.0)
+        drone_controller.interval_scan_loop(tello, frame_reader, step_cm=20.0, min_cov_pct=5.0)
         
         # vypnuti dronu a streamu
         tello.streamoff()
